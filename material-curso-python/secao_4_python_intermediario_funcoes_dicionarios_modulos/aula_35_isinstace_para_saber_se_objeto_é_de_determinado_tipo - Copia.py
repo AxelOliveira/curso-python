f26817507ff18fc830c2278d35a8cd3e5b552e3d
Axel Oliveira Martins <axeloliveira85@gmail.com>
2026-02-05 11:11:47 -0300

    commit

---
+++ material-curso-python/secao_4_python_intermediario_funcoes_dicionarios_modulos/aula_35_isinstace_para_saber_se_objeto_é_de_determinado_tipo - Copia.py
@@ -0,0 +1,24 @@
+# isinstance - para saber se objeto é de determinado tipo
+# No python eu posso checar se alguma coisa é de determinado tipo, usando isinstance(valor, str)
+# Consigo passar uma tupla no is instance(valor, (in, float))
+lista = [
+    'a', 1, 1.1, True, [0, 1, 2], (1, 2), {0, 1}, {'nome': 'Wonwoo'},
+]
+
+for item in lista:
+    if isinstance(item, set):
+        print('SET')
+        item.add(5)
+        print(item, isinstance(item, set))
+
+    elif isinstance(item, str):
+        print('STR')
+        print(item.upper())
+
+    elif isinstance(item, (int, float)):
+        print('NUM')
+        print(item, item * 2)
+
+    else:
+        print('OUTRO')
+        print(item)
