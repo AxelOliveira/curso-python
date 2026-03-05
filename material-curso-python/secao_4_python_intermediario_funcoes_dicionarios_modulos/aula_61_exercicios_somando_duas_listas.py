c7c9a3327c9d42e3171b8520d1670ad34e289f0c
Axel Oliveira Martins <axeloliveira85@gmail.com>
2026-03-05 11:13:03 -0300

    Update aula_61_exercicios_somando_duas_listas.py

--- material-curso-python/secao_4_python_intermediario_funcoes_dicionarios_modulos/aula_61_exercicios_somando_duas_listas.py
+++ material-curso-python/secao_4_python_intermediario_funcoes_dicionarios_modulos/aula_61_exercicios_somando_duas_listas.py
@@ -11,7 +11,32 @@ lista_b     = [1, 2, 3, 4]
 =================== resultado
 lista_soma  = [2, 4, 6, 8]
 """
+
+# ============================================================
+# 1) Criar listas de exemplo
+# ============================================================
+
+# 1 - Lista A (maior)
 lista_a = [1, 2, 3, 4, 5, 6, 7]
+
+# 2 - Lista B (menor)
 lista_b = [1, 2, 3, 4]
+
+# ============================================================
+# 2) Somar valores das listas
+# ============================================================
+
+# 3 - zip junta os elementos das duas listas
+#     formando pares (tuplas)
+#     Exemplo:
+#     (1,1), (2,2), (3,3), (4,4)
+
+# 4 - Para cada par (x, y), somamos os valores
 lista_soma = [x + y for x, y in zip(lista_a, lista_b)]
+
+# ============================================================
+# 3) Mostrar resultado
+# ============================================================
+
+# 5 - Exibir nova lista com valores somados
 print(lista_soma)
